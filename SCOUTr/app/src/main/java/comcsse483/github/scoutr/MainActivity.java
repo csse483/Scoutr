d19cc694f1f884f89b00fef9f0daceee0d293b18
Marc Schmitt <marc.schmitt.mls@gmail.com>
2016-01-18 00:51:16 -0500

    Created android studio project.

---
+++ SCOUTr/app/src/main/java/comcsse483/github/scoutr/MainActivity.java
@@ -0,0 +1,24 @@
+package comcsse483.github.scoutr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private MainMenuAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdapter = new MainMenuAdapter();
+        //Capture RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(mAdapter);
+
+    }
+}
