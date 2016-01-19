7df54281152fff1f68509af183785beded407a43
Knufire <knufire@gmail.com>
2016-01-18 21:30:52 -0500

    Created Team Object.
    Also testing git configuration

---
+++ SCOUTr/app/src/main/java/comcsse483/github/scoutr/Team.java
@@ -0,0 +1,23 @@
+package comcsse483.github.scoutr;
+
+/**
+ * Created by yarlagrt on 1/18/2016.
+ */
+public class Team {
+    private int teamNumber;
+    int autoLowGoalAttempted;
+    int autoLowGoalScored;
+    int lowGoalAttempted;
+    int lowGoalScored;
+    int crossedA1;
+    int crossedA2;
+    int crossedB1;
+    int crossedB2;
+    int crossedC1;
+    int crossedC2;
+    int crossedD1;
+    int crossedD2;
+    boolean towerChallenged;
+    boolean towerScaled;
+
+}
