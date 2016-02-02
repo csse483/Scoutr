06e0c57f87d3460e9af3dc27197548be3ce251a3
Knufire <knufire@gmail.com>
2016-02-01 23:00:59 -0500

    Created SQL Schema and started SQL Helper

---
+++ SCOUTr/app/src/main/java/comcsse483/github/scoutr/DBHelper.java
@@ -0,0 +1,36 @@
+package comcsse483.github.scoutr;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by yarlagrt on 2/1/2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "ScouterDB.db";
+    private static final String TABLE_NAME = "TEAM_MATCH_DATA";
+
+
+
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
