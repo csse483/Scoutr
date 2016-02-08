e705a54aa982e034c1412d07ee42a81b76a37127
Marc Schmitt <marc.schmitt.mls@gmail.com>
2016-02-08 15:24:59 -0500

    RecordDataFragment now captures all ui elements.

--- SCOUTr/app/src/main/java/comcsse483/github/scoutr/fragments/RecordDataFragment.java
+++ SCOUTr/app/src/main/java/comcsse483/github/scoutr/fragments/RecordDataFragment.java
@@ -7,29 +7,49 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import comcsse483.github.scoutr.R;
 import comcsse483.github.scoutr.adapters.RecordDataAdapter;
+import comcsse483.github.scoutr.models.DataContainer;
 
 
 public class RecordDataFragment extends Fragment {
     private RecordDataAdapter mAdapter;
+    private DataContainer mDataContainer;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mDataContainer = new DataContainer();
+        mAdapter = new RecordDataAdapter(getContext(), mDataContainer);
 
-        mAdapter = new RecordDataAdapter(getContext());
-        RecyclerView view = (RecyclerView) inflater.inflate(R.layout.fragment_record_data, container, false);
-        view.setLayoutManager(new LinearLayoutManager(getContext()));
-        view.setAdapter(mAdapter);
+        View view = inflater.inflate(R.layout.fragment_record_data, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.record_data_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(mAdapter);
+
+        Button recordDataButton = (Button) view.findViewById(R.id.record_data_button);
+        recordDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exportData();
+            }
+        });
         return view;
     }
-
-    //TODO: Add Button to Record Data
-    //TODO: Add data container
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
+    }
+
+    /**
+     * Exports the datacontainer.
+     * @return
+     */
+    public DataContainer exportData(){
+        mAdapter.recordData();
+        return mDataContainer;
     }
 }
